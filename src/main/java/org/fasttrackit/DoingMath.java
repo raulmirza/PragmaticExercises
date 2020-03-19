836a869c4d4ef0e8f6d9695d04f4aa83674de693
maraalexandra <mara.alexandra11@yahoo.com>
2020-03-19 13:17:02 +0200

    doing simple math

--- src/main/java/org/fasttrackit/DoingMath.java
+++ src/main/java/org/fasttrackit/DoingMath.java
@@ -8,13 +8,29 @@ public class DoingMath {
 
         Scanner input = new Scanner(System.in);
 
-        int firstNumber = 15, secondNumber=18;
+        int firstNumber = 20, secondNumber=10;
 
         int sum;
 
         sum = firstNumber + secondNumber;
 
-        System.out.println("Sum of two number is:" + sum);
+        int product = firstNumber*secondNumber;
+
+        int product2 = firstNumber/secondNumber;
+
+        int product3 = firstNumber - secondNumber;
+
+
+        System.out.println("Sum of two numbers is:" + sum);
+
+        System.out.println("Multiple of numbers is:" + product);
+
+        System.out.println("Result of dividing the two numbers is: " + product2);
+
+        System.out.println("Difference of two numbers is: " + product3);
+
+
+
 
 
     }
