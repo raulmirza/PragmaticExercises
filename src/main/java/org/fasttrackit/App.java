5a817dc4a701282900a55429c6997c8cced95361
maraalexandra <mara.alexandra11@yahoo.com>
2020-04-05 13:56:16 +0300

    overloading method

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,5 +1,6 @@
 package org.fasttrackit;
 
+import org.fasttrackit.inputprocessingoutput.Numbers;
 import org.fasttrackit.inputprocessingoutput.PrintingQuotes;
 
 import java.util.Scanner;
@@ -11,9 +12,10 @@ import java.util.Scanner;
 public class App {
 
     public static void main( String[] args ) {
-
-
-
+        Numbers.getSum(5,10);
+        Numbers.getDif(14,7);
+        Numbers.getSum(33.5,54.6);
+        Numbers.getDif(15.7,4.6);
 
 
 
--- src/main/java/org/fasttrackit/inputprocessingoutput/Numbers.java
+++ src/main/java/org/fasttrackit/inputprocessingoutput/Numbers.java
@@ -2,7 +2,7 @@ package org.fasttrackit.inputprocessingoutput;
 
 public class Numbers {
 
-    public int getSum(int firstNumber, int secondNumber) {
+    public static int getSum(int firstNumber, int secondNumber) {
 
         int sum = firstNumber + secondNumber;
         sum = firstNumber + secondNumber;
@@ -12,7 +12,7 @@ public class Numbers {
 
     }
 
-    public int getDif(int firstNumber, int secondNumber) {
+    public static int getDif(int firstNumber, int secondNumber) {
         int dif = secondNumber - firstNumber;
         dif = firstNumber - secondNumber;
         System.out.println(dif);
@@ -20,4 +20,17 @@ public class Numbers {
     }
 
 
+    public static double getSum(double firstNumber, double secondNumber){
+
+        double sum = firstNumber + secondNumber;
+        sum = firstNumber +secondNumber;
+        System.out.println(sum);
+        return sum;
+    }
+    public static double getDif(double firstNumber, double secondNumber) {
+        double dif = firstNumber/secondNumber;
+        dif = firstNumber/secondNumber;
+        System.out.println(dif);
+        return dif;
+    }
 }
